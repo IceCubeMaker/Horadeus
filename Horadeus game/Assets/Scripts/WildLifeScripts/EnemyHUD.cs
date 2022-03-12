0ba51c40087487f5c2633b5e4fe7891dca7047a7
TeamKid <shenoybaroi@gmail.com>
2022-03-12 16:24:52 +0600

    Remove Healthbar UI from wildlife If health is full
    Disables canvas element on the UI for Wildlife to remove healthbar.

--- Horadeus game/Assets/Scripts/WildLifeScripts/EnemyHUD.cs
+++ Horadeus game/Assets/Scripts/WildLifeScripts/EnemyHUD.cs
@@ -7,6 +7,7 @@ public class EnemyHUD : MonoBehaviour
 {
     public RectTransform healthbar; //Use to change the x scale to impliment healthbar
     private WildlifeBehaviour parentwlb; //Use for parent WildlifeBehaviour script
+    private Canvas canva;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,24 @@ public class EnemyHUD : MonoBehaviour
         
         GetComponent<Canvas>().worldCamera = Camera.main;
         parentwlb = GetComponentInParent<WildlifeBehaviour>();
+        canva = GetComponent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.forward = Camera.main.transform.forward; //Make Canvas face same direction as camera
+        
+        //Remove healthbar if full health
+        if (parentwlb.health >= parentwlb.totalHealth)
+        {
+            canva.enabled = false;
+            return;
+        }
 
         if (healthbar) //Set healthbar scale
         {
+            canva.enabled = true;
             healthbar.localScale = new Vector3((float)parentwlb.health / parentwlb.totalHealth, 1f, 1f);
         }
     }
